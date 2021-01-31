18c96afded1f61858b162dd14441b09b7079f620
chris-revell <bykzyd-ryzkEt-4roqgu>
2021-01-31 12:18:46 +0000

    Initialise repo

---
+++ Simulate.jl
@@ -0,0 +1,27 @@
+#
+#  Simulate.jl
+#  VertexModelJL
+#
+#  Created by Christopher Revell on 31/01/2021.
+#
+#
+
+module Simulate
+
+# Julia packages
+using LinearAlgebra
+#using Random
+#using Distributions
+#using Base.Threads
+#using StaticArrays
+
+# Local modules
+#using OrthonormalBases
+
+@inline @views function simulate()
+
+end
+
+export simulate
+
+end
---
+++ main.jl
@@ -0,0 +1,17 @@
+#
+#  main.jl
+#  VertexModelJL
+#
+#  Created by Christopher Revell on 31/01/2021.
+#
+#
+
+# Load simulate function
+"./" in LOAD_PATH ? nothing : push!(LOAD_PATH,"./")
+using Simulate
+
+# Define run parameters
+
+
+# Run simulation
+simulate()
