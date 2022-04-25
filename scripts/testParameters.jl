04ebda4af852e1e5d6389d3b5f0eda072e808d79
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-04-25 17:33:49 +0100

    Run script updates

--- scripts/testParameters.jl
+++ scripts/testParameters.jl
@@ -1,12 +1,12 @@
-initialSystem    = "seven"
+initialSystem    = "seven"#"data/sims/AlexPaperParameters/2022-04-22-15-12-34/"
 realTimetMax     = 4.0*86400.0 # = 24 hours
 realCycleTime    = 86400.0 # = 24 hours
-γ                = 0.2
-λ                = -0.3
+γ                = 0.15#0.1#0.2
+λ                = -0.9#-0.1#-0.3
 viscousTimeScale = 20.0
 dt               = 0.5
 preferredArea    = 1.0
-pressureExternal = 0.1
+pressureExternal = 0.0
 outputTotal      = 100
 t1Threshold      = 0.01
 outputToggle     = 1
