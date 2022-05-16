507f6e26c8c99259b473d8f692ecb5a79f0862ce
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-05-16 13:37:46 +0100

    Tidying

--- scripts/testParameters.jl
+++ scripts/testParameters.jl
@@ -1,13 +1,13 @@
-initialSystem    = "seven"#"data/sims/AlexPaperParameters/2022-04-22-15-12-34/"
-realTimetMax     = 5.0*86400.0 # = 24 hours
-realCycleTime    = 86400.0 # = 24 hours
-γ                = 0.2#0.15#0.1
-L₀               = 0.75#3.0#0.5
+initialSystem    = "seven"
+realTimetMax     = 4.0*86400.0
+realCycleTime    = 86400.0      # = 24 hours
+γ                = 0.2          #0.15#0.1
+L₀               = 0.75         #3.0#0.5
 viscousTimeScale = 20.0
-dt               = 0.5
+dt               = 0.1
 A₀               = 1.0
-pressureExternal = 0.0
+pressureExternal = 0.2
 outputTotal      = 100
-t1Threshold      = 0.01
+t1Threshold      = 0.1
 outputToggle     = 1
-plotToggle       = 0
+plotToggle       = 1
