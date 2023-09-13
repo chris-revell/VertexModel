22bd1e8572533ad54e74630962884caa81bfa4d4
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-09-13 11:48:18 +0100

    Update airyStressCAnimation.jl

--- notebooks/airyStressCAnimation.jl
+++ notebooks/airyStressCAnimation.jl
@@ -17,40 +17,42 @@ using Colors
 @from "$(projectdir())/src/Laplacians.jl" using Laplacians
 @from "$(projectdir())/src/Potentials.jl" using Potentials
 
-folderName = "newlongTest/L₀=0.75_realTimetMax=86400.0_t1Threshold=0.01_γ=0.2_23-03-08-20-49-23"
+for f in [f for f in readdir(datadir("sims/examples")) if occursin("γ",f)]
 
-fig = CairoMakie.Figure(resolution=(1000,1000))
-ax = Axis(fig[1,1][1,1],aspect=DataAspect())
-hidedecorations!(ax)
-hidespines!(ax)
-mov = VideoStream(fig, framerate=5)
+    folderName = "sims/examples/$f"
 
-potentials = Vector{Float64}[]
-ψ̆Lims = [0.0,0.0]
-for t=0:100
-    @unpack R, matrices, params = load(datadir(folderName,"frames","systemData$(@sprintf("%03d", t)).jld2"))
-    ψ̆, spectrum = psicPotential(R,params,matrices)
-    push!(potentials,ψ̆)
-    ψ̆Lims .= [-max(maximum(abs.(ψ̆)),maximum(ψ̆Lims)),max(maximum(abs.(ψ̆)),maximum(ψ̆Lims))]
-end 
+    fig = CairoMakie.Figure(resolution=(1000,1000))
+    ax = Axis(fig[1,1][1,1],aspect=DataAspect())
+    hidedecorations!(ax)
+    hidespines!(ax)
+    mov = VideoStream(fig, framerate=5)
 
-Colorbar(fig[1,1][1,2],limits=ψ̆Lims,colormap=:bwr,flipaxis=true)
+    potentials = Vector{Float64}[]
+    ψ̆Lims = [0.0,0.0]
+    for t=1:99
+        @unpack R, matrices, params = load(datadir(folderName,"frames","systemData$(@sprintf("%03d", t)).jld2"))
+        ψ̆, spectrum = psicPotential(R,params,matrices)
+        push!(potentials,ψ̆)
+        ψ̆Lims .= [-max(maximum(abs.(ψ̆)),maximum(ψ̆Lims)),max(maximum(abs.(ψ̆)),maximum(ψ̆Lims))]
+    end 
+
+    Colorbar(fig[1,1][1,2],limits=ψ̆Lims,colormap=:bwr,flipaxis=true)
 
 
-for t=0:100
-    @unpack R, matrices, params = load(datadir(folderName,"frames","systemData$(@sprintf("%03d", t)).jld2"))
-    @unpack B, Bᵀ, C, cellPositions = matrices
-    @unpack nCells = params
+    for t=1:99
+        @unpack R, matrices, params = load(datadir(folderName,"frames","systemData$(@sprintf("%03d", t)).jld2"))
+        @unpack B, Bᵀ, C, cellPositions = matrices
+        @unpack nCells = params
 
-    cellPolygons = makeCellPolygons(R,params,matrices)
-    empty!(ax)
-    ax.title = "t = $(@sprintf("%.2f", t))"
-    xlims!(ax, 1.1*min(minimum(first.(R)), minimum(last.(R))), 1.1*max(maximum(first.(R)), maximum(last.(R))))
-    ylims!(ax, 1.1*min(minimum(first.(R)), minimum(last.(R))), 1.1*max(maximum(first.(R)), maximum(last.(R))))
-    for i=1:nCells
-        poly!(ax,cellPolygons[i],color=[potentials[t+1][i]],colormap=:bwr,colorrange=Tuple(ψ̆Lims), strokecolor=(:black,1.0),strokewidth=5)
+        cellPolygons = makeCellPolygons(R,params,matrices)
+        empty!(ax)
+        ax.title = "t = $(@sprintf("%.2f", t))"
+        for i=1:nCells
+            poly!(ax,cellPolygons[i],color=[potentials[t][i]],colormap=:bwr,colorrange=Tuple(ψ̆Lims), strokecolor=(:black,1.0),strokewidth=5)
+        end
+        reset_limits!(ax)
+        recordframe!(mov)
     end
-    recordframe!(mov)
-end
 
-save(datadir(folderName,"airyStressC.mp4"),mov)
+    save(datadir(folderName,"airyStressC.mp4"),mov)
+end
