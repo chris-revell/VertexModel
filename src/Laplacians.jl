5bea3ab0fd1c84b72d5e8068a4976e823c8309d6
natashacowley <natasha.c.cowley@gmail.com>
2023-10-24 16:42:01 +0100

    Added evolution laplacians and associated objects

--- src/Laplacians.jl
+++ src/Laplacians.jl
@@ -12,6 +12,7 @@ module Laplacians
 using DrWatson
 using LinearAlgebra
 using SparseArrays
+using StaticArrays
 using FromFile
 
 # Local modules
@@ -65,26 +66,90 @@ function makeLt(params,matrices,T,linkTriangleAreas,trapeziumAreas)
     return Lₜ
 end
 
+function makeG(params)
+    @unpack nCells, γ = params
+    G=Diagonal(vcat(fill(1,nCells), fill(γ, nCells)))
+    return G
+end
 
-function makeM(params,matrices)
-    @unpack A, Ā, Aᵀ, B, B̄, Bᵀ,cellAreas,edgeLengths, edgeTangents= matrices
-    @unpack nCells, nEdges, nVerts = params
+function makeM(matrices)
+    @unpack A, Ā, B, B̄, edgeTangents, ϵ = matrices
     #dAdr= - 1/2 Sum j ϵᵢ . Bᵢⱼ Āⱼₖ tⱼ = 1/2 Sum j Ānᵢⱼ = 1/2 B diag(ϵ.t) Ā
-    dAdr=-1/2*(B*Diagonal(eachcol((ϵ*reduce(vcat,transpose(t))')))*Ā)
+    dAdr=-1/2*(B*Diagonal(eachcol((ϵ*reduce(vcat,transpose(edgeTangents))')))*Ā)
     #dLdr=Sum j B̄ᵢⱼ Aⱼₖ t̂ⱼ   = B̄ diag(t̂) A
-    dLdr= B̄*Diagonal(t./norm.(t))*A
+    dLdr= B̄*Diagonal(edgeTangents./norm.(edgeTangents))*A
     
     M=vcat(dAdr,dLdr)
+    dropzeros!(M)
   
     return M
 end
 
 
-function makeL(M)
-    L=tr.(M*M')
-    return L
+function makeEvLc(M)
+    EvLc=tr.(M*M')
+    dropzeros!(EvLc)
+    return EvLc
+end
+
+function makeEvLv(M,G)
+    EvLv=sparse([x' for x in M']*[x' for x in (G*M)])
+    dropzeros!(EvLv)
+    return EvLv
+end
+
+function makeX(params,matrices)
+   
+    @unpack A, Ā, B, B̄, edgeLengths, edgeTangents, ϵ = matrices
+    @unpack nCells, nVerts = params
+   
+    dAdrr = Array{SMatrix{2,2,Float64}}(undef,nCells,nVerts,nVerts)
+    fill!(dAdrr,@SMatrix zeros(2,2))
+    dLdrr = Array{SMatrix{2,2,Float64}}(undef,nCells,nVerts,nVerts)
+    fill!(dLdrr,@SMatrix zeros(2,2))
+
+    n=eachcol((ϵ*reduce(vcat,transpose(edgeTangents))'))
+
+    for k=1:nVerts 
+        for m=1:nVerts
+            for j in nzrange(A,k)
+                for i in nzrange(B,rowvals(A)[j])
+
+                    dAdrr[rowvals(B)[i], k,m ]+= -0.5*B[rowvals(B)[i],rowvals(A)[j]]*Ā[rowvals(A)[j],k]*A[rowvals(A)[j],m].*(ϵ)
+
+                    dLdrr[rowvals(B)[i], k,m ]+=0.5*(B̄[rowvals(B)[i],rowvals(A)[j]]*A[rowvals(A)[j],k]*A[rowvals(A)[j],m]*(n[rowvals(A)[j]]/edgeLengths[rowvals(A)[j]])*(n[rowvals(A)[j]]/edgeLengths[rowvals(A)[j]])')/edgeLengths[rowvals(A)[j]]
+
+                end
+
+            end
+        end
+    end
+
+    X=vcat(dAdrr, dLdrr)
+
+    return X
+end
 
 
-export makeLf, makeLc, makeLv, makeLt
+
+function makeQ(params,matrices,X, M)
+    
+    @unpack cellTensions, cellPressures = matrices
+    @unpack nCells, nVerts = params
+    g=vcat(cellPressures, cellTensions)
+
+    gX=Matrix{SMatrix{2,2,Float64,4}}(undef,nVerts,nVerts)
+    fill!(gX,@SMatrix zeros(2,2))
+    for α=1:2*nCells
+        gX+=g[α]X[α, :,:]
+    end
+
+    Q=[m' for m in M]*(sparse(gX)*[m' for m in M'])
+
+    return Q
+
+end
+
+export makeLf, makeLc, makeLv, makeLt, makeG, makeM, makeEvLc, makeEvLv, makeX, makeQ
 
 end #end module 
