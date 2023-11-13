99c0df93e016a589d0538c359e17d6940adab804
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-11-13 12:26:12 +0000

    Bit more useful directory labelling

--- src/CreateRunDirectory.jl
+++ src/CreateRunDirectory.jl
@@ -23,32 +23,11 @@ function createRunDirectory(R,params,matrices,subFolder)
     @unpack initialSystem,realTimetMax,γ,λ,A₀,pressureExternal,viscousTimeScale,outputTotal,t1Threshold,realCycleTime,nVerts,nCells,nEdges,L₀,outputInterval,tMax,nonDimCycleTime = params
 
     # Create directory for run data labelled with current time.
-    paramsName = @savename L₀ γ t1Threshold realTimetMax
+    paramsName = @savename nCells L₀ γ realTimetMax
     folderName = "$(paramsName)_$(Dates.format(Dates.now(),"yy-mm-dd-HH-MM-SS"))"
     # Create frames subfirectory to store system state at each output time
     mkpath(datadir("sims",subFolder,folderName,"frameImages"))
     mkpath(datadir("sims",subFolder,folderName,"frameData"))
-
-    # Store system parameters.
-    # open(datadir("sims",subFolder,folderName,"conditions.txt"),"w") do conditionsFile
-    #     println(conditionsFile, "initialSystem,     $initialSystem")
-    #     println(conditionsFile, "realTimetMax,      $realTimetMax")
-    #     println(conditionsFile, "realCycleTime,     $realCycleTime")
-    #     println(conditionsFile, "γ,                 $γ")
-    #     println(conditionsFile, "λ,                 $λ")
-    #     println(conditionsFile, "viscousTimeScale,  $viscousTimeScale")
-    #     println(conditionsFile, "A₀,                $A₀")
-    #     println(conditionsFile, "pressureExternal,  $pressureExternal")
-    #     println(conditionsFile, "outputTotal,       $outputTotal")
-    #     println(conditionsFile, "t1Threshold,       $t1Threshold")
-    #     println(conditionsFile, "nVerts,            $nVerts")
-    #     println(conditionsFile, "nCells,            $nCells")
-    #     println(conditionsFile, "nEdges,            $nEdges")
-    #     println(conditionsFile, "L₀,                $L₀")
-    #     println(conditionsFile, "outputInterval,    $outputInterval")
-    #     println(conditionsFile, "tMax,              $tMax")
-    #     println(conditionsFile, "nonDimCycleTime,   $nonDimCycleTime")
-    # end
 
     return folderName
 
