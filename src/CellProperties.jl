a70ff1764c2414ddf4266bf3e1312bcf7b536f89
natashacowley <natasha.c.cowley@gmail.com>
2024-02-06 16:30:39 +0000

    Updated Peff for new energy

--- src/CellProperties.jl
+++ src/CellProperties.jl
@@ -39,11 +39,11 @@ function makeShapeTensors(R,params,matrices)
 end
 
 function getPeff(params, matrices)
-    @unpack cellAreas, cellPerimeters= matrices
+    @unpack cellAreas, cellPerimeters, cellPressures, cellTensions= matrices
     @unpack nCells, γ, L₀= params
 
     Peff=zeros(nCells)
-    Peff=(cellAreas.-1)+(γ.*(cellPerimeters.-L₀).*cellPerimeters)./(2*cellAreas)
+    Peff=cellPressures+(-cellTensions.*cellPerimeters)./(2*cellAreas)
     return Peff
 end
 
@@ -73,7 +73,7 @@ function getShearStress(params, matrices, cellJ)
     @unpack nCells= params
 
     detJ=det.(cellJ)
-    cellShearStress=((cellPerimeters.*cellTensions)./cellAreas).*sqrt.(-detJ)
+    cellShearStress=((cellPerimeters.*(-cellTensions))./cellAreas).*sqrt.(-detJ)
 
     return cellShearStress
 end
