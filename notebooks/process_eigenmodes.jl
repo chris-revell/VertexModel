3dc46578ff53359154a9cb967b9bc2f9d2855cf7
natashacowley <natasha.c.cowley@gmail.com>
2024-02-06 14:01:30 +0000

    updated for new energy

--- notebooks/process_eigenmodes.jl
+++ notebooks/process_eigenmodes.jl
@@ -17,6 +17,7 @@ using JLD2
 using LaTeXStrings
 using Glob
 using Printf
+using ColorSchemes
 
 
 @from "$(projectdir())/src/VertexModelContainers.jl" using VertexModelContainers
@@ -24,83 +25,53 @@ using Printf
 @from "$(projectdir())/src/AnalysisFunctions.jl" using AnalysisFunctions
 @from "$(projectdir())/src/CellProperties.jl" using CellProperties
 
-function makeE(matrices, params)
+folder="C:\\Users\\v35431nc\\Documents\\VM_code\\VertexModel\\data\\sims/new_energy/100_cells/relaxed"
+files=Glob.glob("new_energy/100_cells/relaxed/systemData*.jld2","C:\\Users\\v35431nc\\Documents\\VM_code\\VertexModel\\data\\sims")
+mkpath(datadir(folder,"cell_plots"))
+mkpath(datadir(folder,"spectra"))
+mkpath(datadir(folder,"cell_modes"))
+mkpath(datadir(folder,"vertex_modes"))
 
-
-    @unpack edgeTangents,A, Ā, B, C= matrices
-    @unpack nCells, nEdges, nVerts = params
-
-
-
-    vertexAreas=zeros(nVerts)
-    edgeMidpointLinks=fill(SVector{2, Float64}(zeros(2)), (nCells, nVerts))
-
-    nzC = findnz(C)
-    ikPairs = tuple.(nzC[1],nzC[2])
-    for (i,k) in ikPairs
-        for j=1:nEdges
-            edgeMidpointLinks[i,k] = edgeMidpointLinks[i,k] .+ 0.5.*B[i,j]*edgeTangents[j]*Ā[j,k]
-        end
-    end
-
-
-    ##Check k_is length ==1
-
-    for k=1:nVerts
-
-        k_is = findall(x->x!=0, C[:,k])
-        if length(k_is) == 1
-            edgesSharedBy_i1_And_k = findall(x->x!=0, B[k_is[1],:])∩findall(x->x!=0, A[:,k])
-            vertexAreas[k] = 0.5^3*norm([edgeTangents[edgesSharedBy_i1_And_k[1]]...,0.0]×[edgeTangents[edgesSharedBy_i1_And_k[2]]...,0.0])
-        elseif length(k_is) == 2
-            edgesSharedBy_i1_And_k = findall(x->x!=0, B[k_is[1],:])∩findall(x->x!=0, A[:,k])
-            vertexAreas[k] = 0.5^3*norm([edgeTangents[edgesSharedBy_i1_And_k[1]]...,0.0]×[edgeTangents[edgesSharedBy_i1_And_k[2]]...,0.0])
-            edgesSharedBy_i2_And_k = findall(x->x!=0, B[k_is[2],:])∩findall(x->x!=0, A[:,k])
-            vertexAreas[k] += 0.5^3*norm([edgeTangents[edgesSharedBy_i2_And_k[1]]...,0.0]×[edgeTangents[edgesSharedBy_i2_And_k[2]]...,0.0])
-        else
-            vertexAreas[k] = 0.5*norm([edgeMidpointLinks[k_is[1], k]...,0.0]×[edgeMidpointLinks[k_is[2],k]...,0.0])
-        end
-    end
-
-    E=Diagonal(vertexAreas)
-
-    return(E)
-
-end
-
-folders=Glob.glob("relax_100_cells_fluid/*L₀*","C:\\Users\\v35431nc\\Documents\\VM_code\\VertexModel\\data\\sims" )
-
-for f in folders
+mkpath(datadir(folder,"data"))
+for f in files
     print(f)
-    @unpack R, matrices, params = load(datadir(f,"frameData/systemDataFinal.jld2"))
-    @unpack B, Bᵀ, C, cellPositions, cellAreas, cellPerimeters, cellPressures, cellTensions = matrices
-    @unpack nCells,nVerts = params
+    @unpack R, matrices, params = load(f)
+    @unpack nCells,nVerts, γ, L₀ = params
+    @unpack cellAreas,cellPerimeters, cellTensions, cellPressures, cellEdgeCount, edgeTangents,vertexAreas,A,Ā,B,B̄,C, ϵ = matrices
 
-    mkpath(datadir(f,"eigenmodes"))
+
     print(' ')
     print(nCells)
     print('\n')
 
-    E=makeE(matrices, params)
-    Eflat=kron(E,Matrix(1.0I, 2, 2))
-    G=makeG(params)
+    G=makeGnew(params, matrices) #update for new model
     M=makeM(matrices)
-    Lv=makeEvLv(M, G)
+    Lv=makeEvLvnew(M, G, vertexAreas)
+    Lc=makeEvLcnew(M, G, vertexAreas)
     X=makeX(params, matrices)
     g=vcat(cellPressures, -cellTensions)
-
+    
     gX=Matrix{SMatrix{2,2,Float64,4}}(undef,nVerts,nVerts)
     fill!(gX,@SMatrix zeros(2,2))
-
-    for α=1:2*nCells
-        gX+=g[α]X[α, :,:]
-    end
     
-    LcE=Matrix(tr.(M*(inv(E)*M'))*G)
+    for α=1:2*nCells
+         gX+=g[α]X[α, :,:]
+    end
+
+    dAdr=-1/2*B*Diagonal([(ϵ*T) for T in edgeTangents])*Ā
+    dLdr= B̄*Diagonal((edgeTangents)./norm.(edgeTangents))*A
+
+    Mtemp=vcat(dAdr,dLdr)
+    Mflat=transpose(reshape(reinterpret(Float64,[vcat(Matrix(Mtemp)[x,:]...) for x in collect(1:2*nCells)]), ( 2*nVerts, 2*nCells)))
+        
+    E=Diagonal(vertexAreas)
+    Eflat=kron(E,Matrix(1.0I, 2, 2))
+    
+    LcE=Matrix(Lc)
     evalLc,evecLctemp=LAPACK.syev!('V','U',deepcopy(sqrt(G)*Matrix(LcE)*inv(sqrt(G))))
     evecLc=inv(sqrt(G))*evecLctemp
     
-    LvE=inv(E)*Lv
+    LvE=Matrix(Lv)
     evalLv,evecLvtemp=LAPACK.syev!('V','U',deepcopy(sqrt(Eflat)*Matrix(mortar(LvE))*inv(sqrt(Eflat))))
     evecLv=inv(sqrt(Eflat))*evecLvtemp
     
@@ -108,9 +79,10 @@ for f in folders
     evalH,evecHtemp=LAPACK.syev!('V','U',deepcopy(sqrt(Eflat)*H*inv(sqrt(Eflat))))
     evecH=inv(sqrt(Eflat))*evecHtemp
     
-    Ntemp=Matrix(sqrt.(G)*M*inv(sqrt.(E)))
-    Ntempflat=transpose(reshape(reinterpret(Float64,[vcat(Matrix(Ntemp)[x,:]...) for x in collect(1:2*nCells)]), ( 2*nVerts, 2*nCells)))
-    UNF,sNF,VNF=svd(Ntempflat, full=true)
+    #Ntemp=Matrix(sqrt.(G)*M*inv(sqrt.(E)))
+    # Ntempflat=transpose(reshape(reinterpret(Float64,[vcat(Matrix(Ntemp)[x,:]...) for x in collect(1:2*nCells)]), ( 2*nVerts, 2*nCells)))
+    Nflat=sqrt.(G)*Mflat*inv(sqrt(Eflat))
+    UNF,sNF,VNF=svd(Nflat, full=true)
     
     Y=inv(sqrt.(G))*UNF
     Z=inv(sqrt.(Eflat))*VNF
@@ -119,35 +91,74 @@ for f in folders
     DD=D + Diagonal(evalLv)
     evalDD,evecDD=LAPACK.syev!('V','U',deepcopy(DD))
 
-    evecmap=[evecDD[:,x]'*Eflat*(evecLv'*Matrix(mortar(gX))*evecLv+Diagonal(evalLv))*evecDD[:,x] for x in 1:2nVerts]
-    evmapLv=[evecDD[:,x]'*Eflat*(Diagonal(evalLv))*evecDD[:,x] for x in 1:2nVerts]
-    evmapgX=[evecDD[:,x]'*Eflat*(evecLv'*Matrix(mortar(gX))*evecLv)*evecDD[:,x] for x in 1:2nVerts]
+    evecmap=[evecDD[:,x]'*(evecLv'*Matrix(mortar(gX))*evecLv+Diagonal(evalLv))*evecDD[:,x] for x in 1:2nVerts]
+    evmapLv=[evecDD[:,x]'*(Diagonal(evalLv))*evecDD[:,x] for x in 1:2nVerts]
+    evmapgX=[evecDD[:,x]'*(evecLv'*Matrix(mortar(gX))*evecLv)*evecDD[:,x] for x in 1:2nVerts]
 
-    writedlm(datadir(f,"eigenmodes","LcE_eigenvalues.csv"),evalLc, ',') 
-    writedlm(datadir(f,"eigenmodes","LcE_eigenvectors.csv"),evecLc, ',') 
-    writedlm(datadir(f,"eigenmodes","LvE_eigenvalues.csv"),evalLv, ',') 
-    writedlm(datadir(f,"eigenmodes","LvE_eigenvectors.csv"), evecLv, ',') 
-    writedlm(datadir(f,"eigenmodes","HE_eigenvalues.csv"), evalH, ',') 
-    writedlm(datadir(f,"eigenmodes","HE_eigenvectors.csv"), evecH, ',') 
-    writedlm(datadir(f,"eigenmodes","DppE_tot_diag_sigma_eigenvalues.csv"), evalDD, ',') 
-    writedlm(datadir(f,"eigenmodes","DppE_tot_diag_sigma_eigenvectors.csv"), evecDD, ',') 
-    writedlm(datadir(f,"eigenmodes","svd_N_Y.csv"), Y, ',') 
-    writedlm(datadir(f,"eigenmodes","svd_N_Z.csv"), Z, ',') 
-    writedlm(datadir(f,"eigenmodes","svd_N_sigma.csv"), sNF, ',') 
+
+
+    dat_dir=mkpath(datadir(folder,"data", "Γ_"*string(params.γ)*"_L0_"*string(params.L₀)))
+
+    writedlm(datadir(dat_dir,"LcE_eigenvalues.csv"), evalLc, ',') 
+    writedlm(datadir(dat_dir,"LcE_eigenvectors.csv"), evecLc, ',') 
+    writedlm(datadir(dat_dir,"LvE_eigenvalues.csv"), evalLv, ',') 
+    writedlm(datadir(dat_dir,"LvE_eigenvectors.csv"), evecLv, ',') 
+    writedlm(datadir(dat_dir,"HE_eigenvalues.csv"), evalH, ',') 
+    writedlm(datadir(dat_dir,"HE_eigenvectors.csv"), evecH, ',') 
+    writedlm(datadir(dat_dir,"DppE_tot_diag_sigma_eigenvalues.csv"), evalDD, ',') 
+    writedlm(datadir(dat_dir,"DppE_tot_diag_sigma_eigenvectors.csv"), evecDD, ',') 
+    writedlm(datadir(dat_dir,"svd_N_Y.csv"), Y, ',') 
+    writedlm(datadir(dat_dir,"svd_N_Z.csv"), Z, ',') 
+    writedlm(datadir(dat_dir,"svd_N_sigma.csv"), sNF, ',') 
 
     nv=LinRange(1, 2*nVerts, 2*nVerts)
     nc=LinRange(2*nVerts-2*nCells+1, 2*nVerts, 2*nCells)
 
     fig = Figure()
     ax=Axis(fig[1, 1], xlabel="n", ylabel="log₁₀λₙ", title="Γ = "*string(params.γ)*", L₀ = "*string(params.L₀))
-    
-    scatter!(ax,nc, log10.(sort(abs.(sNF.^2))), color=:black,markersize=4, label="σ²ₙ, N")
-    scatter!(ax,nv, log10.(sort(abs.(eigvals(H)))), color=:blue,markersize=4, label="λₙ, E⁻¹MᵀGM+E⁻¹gX")
-    scatter!(ax,nv, log10.(sort(abs.(evalDD))), color=:red,markersize=4, label="λₙ D+diag(σ²ₙ)")#
+
+    scatter!(ax,nv, log10.(sort(abs.(evalH))), color=:black,markersize=4, label=L"\lambda_n,\quad \mathcal{H}")
+    scatter!(ax,nv, log10.(sort(abs.(evalLv))), color=:blue,markersize=4, label=L"\lambda_n,\quad \mathcal{L}_v^G")
+    scatter!(ax,nc, log10.((abs.(reverse(sNF.^2)))), color=:red,markersize=4, label="σ²ₙ, N")
     
     #vlines!(ax,2*nVerts+1-((2*nCells)-1), color=:red)
     fig[1, 2] = Legend(fig, ax, framevisible = false)
-    save(datadir(f,"eigenmodes","compare_eigenvalues_E_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
+    save(datadir(folder, "spectra","compare_spectra_log_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
+   
+    fig = Figure()
+    set_theme!(figure_padding=5, backgroundcolor=(:white,1.0), font="Helvetica", fontsize=19)
+    ax=Axis(fig[1, 1], xlabel="Mode number, n", ylabel="λₙ", yscale=log10, title="Γ = "*string(params.γ)*", L₀ = "*string(params.L₀))
+    hidedecorations!(ax, grid=true, ticks=false, label=false,ticklabels = false)
+    #vspan!(197.5, 396.5, color = (:grey, 0.3))
+    scatter!(ax,nv[4:end], ((abs.(evalH)))[4:end], color=:black,markersize=5, label=L"\lambda_n,\, \mathcal{H}")
+
+    #scatter!(ax,nv, (sort(abs.(evalLv))), color=ColorSchemes.seaborn_colorblind6[1],markersize=4, label=L"\lambda_n,\, \mathcal{L}_v^G")
+
+
+    scatter!(ax,nv[4:end], ((abs.(evmapLv)))[4:end], color=ColorSchemes.seaborn_colorblind6[2],markersize=5)
+    scatter!(ax,nv[4:end], ((abs.(evmapgX)))[4:end], color=ColorSchemes.seaborn_colorblind6[3],markersize=5)
+
+
+    elem_1 = [MarkerElement(color = :black, marker = :circle, markersize = 15)]
+
+    elem_2 = [MarkerElement(color = ColorSchemes.seaborn_colorblind6[2], marker = :circle, markersize = 15)]
+
+    elem_3 = [MarkerElement(color =ColorSchemes.seaborn_colorblind6[3], marker = :circle, markersize = 15)]
+
+    # # elem_4 = [MarkerElement(color =ColorSchemes.seaborn_colorblind6[3], marker = :circle, markersize = 12)]
+    Legend(fig[1, 2],
+        [elem_1, elem_2, elem_3],
+        [L"\lambda_n,\quad \mathcal{H}","Material","Geometric"],
+        patchsize = (35, 35), rowgap = 10, framevisible = false)
+    resize_to_layout!(fig)
+
+
+
+    #fig[1, 2] = Legend(fig, ax, framevisible = false)
+    save(datadir(folder,"spectra","Mode_contributions_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
+    
+    mkpath(datadir(folder,"cell_modes"))
+    mode_dir=mkpath(datadir(folder,"cell_modes", "Γ_"*string(params.γ)*"_L0_"*string(params.L₀)))
 
     cellPolygons = makeCellPolygons(R,params,matrices)
 
@@ -184,7 +195,7 @@ for f in folders
         Label(fig[2,1,Left()],string(L"Perimeter"),fontsize = 32, rotation=π/2)
         #Label( fig[0,:],"Γ = "*string(params.γ)*", L₀ = "*string(params.L₀)*", δL = "*string(params.δL),fontsize = 32, color = (:black, 1))
         resize_to_layout!(fig)
-        save(datadir(f,"eigenmodes","eigenmodes_Y$(@sprintf("%03d", 2*nCells-(n-1))).png"),fig)
+        save(datadir(mode_dir,"eigenmodes_Y$(@sprintf("%03d", 2*nCells-(n-1))).png"),fig)
     
         #display(fig)
     end
@@ -220,7 +231,7 @@ for f in folders
     Label(fig[2,1,Bottom()],string(L"Perimeter"),fontsize = 32, rotation=0)
     #Label( fig[0,:],"Γ = "*string(params.γ)*", L₀ = "*string(params.L₀)*", δL = "*string(params.δL),fontsize = 32, color = (:black, 1))
     resize_to_layout!(fig)
-    save(datadir(f,"eigenmodes","Area_Perimeter.png"),fig)
+    save(datadir(folder,"cell_plots","Area_Perimeter_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
 
     cellShapeTensors=makeShapeTensors(R,params, matrices)
     circularity=getCircularity(params, cellShapeTensors)
@@ -257,7 +268,7 @@ for f in folders
     Label(fig[2,1,Bottom()],"Shape parameter",fontsize = 26, rotation=0)
     #Label( fig[0,:],"Γ = "*string(params.γ)*", L₀ = "*string(params.L₀)*", δL = "*string(params.δL),fontsize = 32, color = (:black, 1))
     resize_to_layout!(fig)
-    save(datadir(f,"eigenmodes","circularity_shape.png"),fig)
+    save(datadir(folder,"cell_plots","circularity_shape_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
 
     Peff=getPeff(params, matrices)
     cellQ, cellJ=makeCellQandJ(params, matrices)
@@ -310,7 +321,41 @@ for f in folders
     Label(fig[2,3,Bottom()],"Shear Stress",fontsize = 32, rotation=0)
     Label( fig[0,:],"Γ = "*string(params.γ)*", L₀ = "*string(params.L₀),fontsize = 32, color = (:black, 1))
     resize_to_layout!(fig)
-    save(datadir(f,"eigenmodes","Stress.png"),fig)
+    save(datadir(folder,"cell_plots","stress_pressure_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
+
+    modeSign=zeros(2*nCells)
+    mode_mag_plus=zeros(2*nCells)
+    mode_mag_minus=zeros(2*nCells)
+    mode_magL_plus=zeros(2*nCells)
+    mode_magL_minus=zeros(2*nCells)
+    mode_magA_plus=zeros(2*nCells)
+    mode_magA_minus=zeros(2*nCells)
+    for n=1:2*nCells
+        a=evecLc[1:nCells,n].*evecLc[nCells+1:end,n]
+        bA=evecLc[1:nCells,n]
+        bL=evecLc[nCells+1:end,n]
+        modeSign[n]=length(a[a.>0])
+        mode_mag_plus[n]=sum(a[a.>0])
+        mode_mag_minus[n]=sum(a[a.<0])
+        mode_magL_plus[n]=sum(bL[a.>0])
+        mode_magL_minus[n]=sum(bL[a.<0])
+        mode_magA_plus[n]=sum(bA[a.>0])
+        mode_magA_minus[n]=sum(bA[a.<0])
+    end
+    n=LinRange(1, 2*nCells, 2*nCells)
+    fig = Figure()
+    ax=Axis(fig[1, 1], xlabel="n", ylabel="Count", title="Number of cells with the same sign A and L modes")
+    
+    #scatter!(ax,n, mode_magL_plus, color=:red, label="Σ L modes, same sign", markersize=7)
+    scatter!(ax,n, modeSign, color=:black, label="Σ L modes, opposite", markersize=7)
+    # scatter!(ax,n, mode_magA_plus.+ mode_magL_plus, color=:red, label="Σ same sign", markersize=7)
+    # scatter!(ax,n, mode_magA_minus.+ mode_magL_minus, color=:blue, label="Σ opposite sign", markersize=7)
+    
+    #vlines!(ax,2*nVerts-(2*nCells) +0.5, color=:red)
+    vlines!(ax,nCells+0.5, color=:red)
+    #fig[1, 2] = Legend(fig, ax, framevisible = false)
+    save(datadir(folder,"cell_modes","mode_sign_Γ_"*string(params.γ)*"_L0_"*string(params.L₀)*".png"),fig)
+    
 
 
 end
