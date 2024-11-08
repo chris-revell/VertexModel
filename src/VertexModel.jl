cdcbd685d78413d12f0425390ed44a968f4d29eb
natashacowley <natasha.c.cowley@gmail.com>
2024-11-08 11:31:19 +0000

    pass H to integrator

--- src/VertexModel.jl
+++ src/VertexModel.jl
@@ -14,6 +14,7 @@ using DrWatson
 using FromFile
 using DifferentialEquations
 using DiffEqCallbacks
+using BoundaryValueDiffEq
 using DelimitedFiles
 using LinearAlgebra
 using JLD2
@@ -22,6 +23,7 @@ using StaticArrays
 using CairoMakie
 using Printf
 using CSV
+using NonlinearSolve
 
 # Local modules
 @from "CreateRunDirectory.jl" using CreateRunDirectory
@@ -53,13 +55,13 @@ function conditionSteadyState(u, t, integrator)
     # norm.() calculates magnitudes of all gradients as Floats; 
     # maximum() finds biggest gradient
     # Return true if biggest gradient is below threshold 
-    @show maximum(norm.(get_du(integrator)))
-    (maximum(norm.(get_du(integrator))) < 1e-6) & (integrator.p[1].nCells>=2048)   ? true : false
+    #@show maximum(norm.(get_du(integrator)))
+    (maximum(norm.(get_du(integrator))) < 1e-6) & (integrator.p[1].nCells>=1)   ? true : false
     # Use integrator.opts.abstol as threshold?
 end
 
 function conditionMaxCells(u, t, integrator)
-    (integrator.p[1].nCells>=2048)   ? true : false
+    (integrator.p[1].nCells>=1)   ? true : false
 end
 
 function affectTerminate!(integrator)
@@ -71,6 +73,28 @@ end
 cb = DiscreteCallback(conditionSteadyState, affectTerminate!)
 cb2 = DiscreteCallback(conditionMaxCells, affectTerminate!)
 
+# function bc1!(residual, u, p, t)
+
+#     params, matrices = p
+#     @unpack A,
+#         B,
+#         Ā,
+#         B̄,
+#         edgeLengths,
+#         edgeTangents,
+#         cellHeights,
+#         ϵ,
+#         vertexAreas = matrices
+#     @unpack nVerts,
+#         nCells,
+#         nEdges = params
+
+#     spatialData!(u, params, matrices)
+#     dAdr=-1/2*B*Diagonal([(ϵ*T)' for T in edgeTangents])*Ā
+
+#     residual .= (dAdr'*cellHeights)./vertexAreas
+#     @show residual
+# end
 
 function vertexModel(;
     initialSystem="large",
@@ -85,7 +109,7 @@ function vertexModel(;
     viscousTimeScale=1000.0,
     pressureExternal=0.0,
     peripheralTension=0.0,
-    t1Threshold=0.05,    
+    t1Threshold=0.01,    
     #solver= Vern7(lazy=false),
     #solver= DP8(),
     solver= Tsit5(),
@@ -110,7 +134,7 @@ function vertexModel(;
 
     # realTimetMax=nCycles*realCycleTime
     # Set up initial system, packaging parameters and matrices for system into params and matrices containers from VertexModelContainers.jl
-    R, params, matrices = initialise(initialSystem, realTimetMax, γ, L₀, A₀, pressureExternal, viscousTimeScale, outputTotal, t1Threshold, realCycleTime, peripheralTension, setRandomSeed)
+    R, params, matrices = initialise(initialSystem, realTimetMax, Γa, ΓA, ΓL, L₀, A₀, pressureExternal, viscousTimeScale, outputTotal, t1Threshold, realCycleTime, peripheralTension, setRandomSeed)
 
     # Set up output if outputToggle argument == 1
     if outputToggle==1
@@ -121,13 +145,20 @@ function vertexModel(;
             fig, ax1, mov = plotSetup(R, params, matrices, subFolder, folderName)
         end
     end
+    R=R.*(1.0) #needed to allow me to pusH to R
+    H=SVector(0.5,0.0)
 
+    RH=push!(R,H)    
+
+    @show(RH)
     
-    prob=ODEProblem(model!,R,(0.0,Inf),(params,matrices))
+    prob=ODEProblem(model!,RH,(0.0,Inf),(params,matrices))
+    #bvp1=BVProblem(model!,bc1!,R,(0.0,Inf),(params,matrices) )
 
     #integrator = init(prob,solver,abstol=1e-8, reltol=1e-6, callback=CallbackSet(cb, cb2)) # Adjust tolerances if you notice unbalanced forces in system that should be at equilibrium
 
-    integrator = init(prob,solver,abstol=1e-10, reltol=1e-8, callback=cb) # Adjust tolerances if you notice unbalanced forces in system that should be at equilibrium
+    integrator = init(prob,solver,abstol=1e-8, reltol=1e-8, callback=cb) # Adjust tolerances if you notice unbalanced forces in system that should be at equilibrium
+    #integrator = init(bvp1,Shooting(solver),abstol=1e-10, reltol=1e-8, callback=cb) # Adjust tolerances if you notice unbalanced forces in system that should be at equilibrium
 
     # Iterate until integrator time reaches max system time 
     while integrator.t < params.tMax && integrator.sol.retcode == ReturnCode.Default
@@ -141,20 +172,25 @@ function vertexModel(;
             if frameDataToggle == 1
                 # In order to label vertex locations as "R" in data output, create a view of (reference to) integrator.u named R 
 
-                R = @view integrator.u[:]
+                R = @view integrator.u[1:params.nVerts]
 
                 jldsave(datadir("sims",subFolder,folderName,"frameData","systemData$(@sprintf("%03d", integrator.t*outputTotal÷params.tMax)).jld2");matrices,params,R)            
 
-                e_tot=energy(params,matrices)
-                csvfile=open(datadir("sims",subFolder,folderName,"EnergyTotal.csv"), "a")
-                println(csvfile, string(integrator.t*outputTotal÷params.tMax), ",",e_tot)
-                close(csvfile)
-                print(e_tot ,'\n')
+                # e_tot=energy(params,matrices)
+                # csvfile=open(datadir("sims",subFolder,folderName,"EnergyTotal.csv"), "a")
+                # println(csvfile, string(integrator.t*outputTotal÷params.tMax), ",",e_tot)
+                # close(csvfile)
+                # print(e_tot ,'\n')
                 @show params.nCells
+                @show mean(matrices.cellAreas)
+                @show mean(matrices.cellHeights)
+                #@show integrator.u[end]
+
+
             end
             if frameImageToggle == 1 || videoToggle == 1
                 # Render visualisation of system and add frame to movie
-                visualise(integrator.u, integrator.t, fig, ax1, mov, params, matrices, plotCells, scatterEdges, scatterVertices, scatterCells, plotForces, plotEdgeMidpointLinks)
+                visualise(integrator.u[1:params.nVerts], integrator.t, fig, ax1, mov, params, matrices, plotCells, scatterEdges, scatterVertices, scatterCells, plotForces, plotEdgeMidpointLinks)
             end
             # Save still image of this time step 
             frameImageToggle == 1 ? save(datadir("sims", subFolder, folderName, "frameImages", "frameImage$(@sprintf("%03d", integrator.t*outputTotal÷params.tMax)).png"), fig) : nothing
@@ -162,15 +198,16 @@ function vertexModel(;
         # Step integrator forwards in time to update vertex positions 
         step!(integrator)
 
+        #=
         #Check system for T1 transitions 
-        if t1Transitions!(integrator.u, params, matrices) > 0
+        if t1Transitions!(integrator.u[1:params.nVerts], params, matrices) > 0
             u_modified!(integrator, true)
             # senseCheck(matrices.A, matrices.B; marker="T1") # Check for nonzero values in B*A indicating error in incidence matrices           
             topologyChange!(matrices) # Update system matrices after T1 transition
             spatialData!(integrator.u, params, matrices) # Update spatial data after T1 transition  
         end
        
-        if params.nCells < 2048
+        if params.nCells < 1
             if division!(integrator,params,matrices)>0
                 u_modified!(integrator,true)
                 # senseCheck(matrices.A, matrices.B; marker="division") # Check for nonzero values in B*A indicating error in incidence matrices          
@@ -180,7 +217,7 @@ function vertexModel(;
         end
         #@show params.nCells
 
-
+        =#
        
         
         # Update cell ages with (variable) timestep used in integration step
@@ -195,14 +232,14 @@ function vertexModel(;
         printToggle == 1 ? println("$(@sprintf("%.3f", integrator.t))/$(@sprintf("%.3f", params.tMax)), $(integrator.t*outputTotal÷params.tMax)/$outputTotal") : nothing
         # Save final data file regardless of whether other timepoint data files are saved
         # In order to label vertex locations as "R" in data output, create a view of (reference to) integrator.u named R 
-        R = @view integrator.u[:]
-        fname=@savename L₀ γ
+        R = @view integrator.u[1:params.nVerts]
+        fname=@savename L₀ Γa ΓA ΓL
         jldsave(datadir("sims",subFolder,folderName,"frameData","systemData$(@sprintf("%03d", integrator.t*outputTotal÷params.tMax)).jld2");matrices,params,R)
         jldsave(datadir("sims",subFolder,folderName,"systemDataFinal_$(fname).jld2");matrices,params,R)
 
         if frameImageToggle==1 || videoToggle==1
             # Render visualisation of system and add frame to movie
-            visualise(integrator.u, integrator.t, fig, ax1, mov, params, matrices, plotCells, scatterEdges, scatterVertices, scatterCells, plotForces, plotEdgeMidpointLinks)
+            visualise(integrator.u[1:params.nVerts], integrator.t, fig, ax1, mov, params, matrices, plotCells, scatterEdges, scatterVertices, scatterCells, plotForces, plotEdgeMidpointLinks)
         end
         # Save still image of this time step 
         frameImageToggle == 1 ? save(datadir("sims", subFolder, folderName, "frameImages", "frameImage$(@sprintf("%03d", integrator.t*outputTotal÷params.tMax)).png"), fig) : nothing
