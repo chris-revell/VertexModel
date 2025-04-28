09db037abc415a82d4884abd5ddfcd787a2f2b94
Christopher Revell <christopher.revell@manchester.ac.uk>
2025-04-28 15:55:53 +0100

    Update Energy.jl

--- src/Energy.jl
+++ src/Energy.jl
@@ -13,23 +13,25 @@ module Energy
 using LinearAlgebra
 using UnPack
 
+# Energy per Cowley et al. 2024 Section 2a
+𝒰(θ) = θ*(log(θ)-1.0)
+Uᵢ(Aᵢ, A₀, Lᵢ, L₀, μᵢ, Γᵢ) = μᵢ*(𝒰(Aᵢ/A₀) + Γᵢ*L₀^2*𝒰(Lᵢ/L₀))
+
 function energy(params,matrices)
 
-    @unpack cellEnergies,
-        cellAreas,
-        cellPerimeters = matrices
-    @unpack nCells,
-        A₀,
-        L₀,
-        γ = params
-
+    @unpack cellAreas,
+        cellA₀s,
+        cellPerimeters,
+        cellL₀s,
+        μ,
+        Γ = matrices
+    
     energyTotal = 0.0
     for i = 1:nCells
-        energyTotal += 0.5 * (cellAreas[i] - A₀)^2 + 0.5 * γ * (cellPerimeters[i] - L₀)^2
+        # energyTotal += 0.5 * (cellAreas[i] - A₀)^2 + 0.5 * γ * (cellPerimeters[i] - L₀)^2
+        energyTotal += Uᵢ.(cellAreas[i], cellA₀s[i], cellPerimeters[i], cellL₀s[i], μ[i], Γ[i])
     end
-
     return energyTotal
-
 end
 
 export energy
